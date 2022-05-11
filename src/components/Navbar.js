dbe540b513f86c20c86c623527b221b5906f2b2c
Hudayi <hudayi.fr@gmail.com>
2022-05-11 14:42:22 +0300

    require

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="nav-container">
-      <img src={"../../logo.png"} alt="logo" className="logo" />
+      <img src={require("../../logo.png")} alt="logo" className="logo" />
       <div className="nav-center">
         <h2>Counter React App</h2>
       </div>

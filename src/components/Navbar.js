8e4b13700c1c487deaf3b4481281fe34f601f347
Hudayi <hudayi.fr@gmail.com>
2022-05-22 18:43:53 +0300

    deleted logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 const Navbar = () => {
   return (
     <nav className="nav-container">
-      <img src="../logo.png" alt="logo" className="logo" />
       <div className="nav-center">
         <h2>Counter React App</h2>
       </div>
